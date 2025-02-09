d175bc0155de33ef3632a24e4040ea80d30390db
C0deInBlack <141534149+C0deInBlack@users.noreply.github.com>
2025-02-08 18:28:40 -0700

    Update and rename main.py to main_selenium.py

--- main_selenium.py
+++ main_selenium.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python 
 
 import sys, signal, time, getpass, os
+# import cv2
+# sys.path.append('libs/lib/python3.13/site-packages')
 from selenium import webdriver 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys 
@@ -17,6 +19,8 @@ driver_options.add_argument("--headless")
 browser = webdriver.Firefox(options=driver_options)
 
 url: str = "https://web.whatsapp.com/send?phone="
+selector: str = "div.lexical-rich-text-input:nth-child(2) > div:nth-child(1)"
+button: str = "button.x1c4vz4f"
 
 def main() -> None:
     start_time = time.time()
@@ -26,23 +30,24 @@ def main() -> None:
     
         time.sleep(10)
         browser.save_screenshot('qr.png')
+        # cv2.imshow('QR', (cv2.imread('qr.png', cv2.IMREAD_ANYCOLOR)))
         os.system('kitten icat qr.png')
-        a = getpass.getpass(prompt="\n[*] Type any key after scan the QR: ")
+        a = getpass.getpass(prompt="[*] Type any key after scan the QR: ")
         
         try:
             WebDriverWait(browser, 60).until(EC.element_to_be_clickable((
-                By.CSS_SELECTOR, '.x12lumcd > div:nth-child(1) > div:nth-child(1)')))
+                By.CSS_SELECTOR, 'div.lexical-rich-text-input:nth-child(2) > div:nth-child(1) > p:nth-child(1)')))
         finally:
             time.sleep(10)
-            print("\n[*] Starting attack\n")
+            print("[*] Starting attack\n")
             for i in range(int(sys.argv[2])):
-                browser.find_element(By.CSS_SELECTOR, '.x12lumcd > div:nth-child(1) > div:nth-child(1)').send_keys(str(sys.argv[3]))
-                browser.find_element(By.CSS_SELECTOR, 'button.x1c4vz4f').click()
+                browser.find_element(By.CSS_SELECTOR, selector).send_keys(str(sys.argv[3]))
+                browser.find_element(By.CSS_SELECTOR, button).click()
 
         browser.quit()
         end_time = time.time()
         os.remove(os.path.join(os.getcwd(), 'qr.png'))
-        print("\n[*] Process finished in: %d s\n" % ((end_time - start_time)))
+        print("[*] Process finished in: %d s\n" % ((end_time - start_time)))
 
 if __name__ == '__main__':
     main()
